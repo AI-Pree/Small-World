e7daf55406198af78ba38521aed7a091ca94e101
ai-pree <sexypistolcs@gmail.com>
2021-07-30 21:22:41 +1000

    added hex grid base code and set new goals

---
+++ Assets/Scripts/Algo/Ploygon.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ploygon : MonoBehaviour
+{
+    public struct Point {
+        // point fields for the hexagon
+        int x;
+        int y;
+        int z;
+
+        public Point (int x,  int y ) {
+            this.x  = x;
+            this.y = y;
+        }
+    }
+
+    
+    public struct Hex {
+        public Hex () {
+
+        }
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- README.md
+++ README.md
@@ -4,4 +4,6 @@ Procedural Generation of overworlds and landscape
 
 ## Goals
 - Use Wave Function Collapse algorithm for procedural generation of landscapes and populate the 3D model.
-- Implement Multiplayer feature in it.
+- Try to add seasons for the environment change.
+- able to add different landscape.
+- landscape are implemented by a single grid. Landscape can be full of trees, small civilization of different mythology, deserts, land covered with snow, glaciers 
